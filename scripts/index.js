132cc2e8e0671d0d20b1a0a257e17028b2ef189b
Sergey Pyshkin <sergeypyschkin@yandex.ru>
2022-10-02 00:14:04 +0500

    fix: доработка первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,32 @@
-let openPopupButton = document.querySelector('.profile__edit-button'); //кнопка открытия попапа
-let closePopupButton = document.querySelector('.popup__close'); //кнопка закрытия попапа
-let popup = document.querySelector('.popup'); //Попап редактирования профиля
-let nameProfile = document.querySelector('.profile__title'); //имя
-let jobProfile = document.querySelector('.profile__subtitle');//о себе
-let namePopup = document.querySelector('#name');//поле "имя" попапа
-let jobPopup = document.querySelector('#job');//поле "Род деятельности"
-let form = document.querySelector('.popup__form'); //форма
+let openPopupButton = document.querySelector(".profile__edit-button"); //кнопка открытия попапа
+let closePopupButton = document.querySelector(".popup__close"); //кнопка закрытия попапа
+let popup = document.querySelector(".popup"); //Попап редактирования профиля
+let nameProfile = document.querySelector(".profile__title"); //имя
+let jobProfile = document.querySelector(".profile__subtitle"); //о себе
+let namePopup = document.querySelector("#name"); //поле "имя" попапа
+let jobPopup = document.querySelector("#job"); //поле "Род деятельности"
+let form = document.querySelector(".popup__form"); //форма
 
 //Функция открытия/закрытия попапа
-function openPopup() {
-  if (popup.classList.contains('popup__opened') === false) {
+function togglePopup() {
+  if (popup.classList.contains("popup_opened") === false) {
     namePopup.value = nameProfile.textContent;
     jobPopup.value = jobProfile.textContent;
-    popup.classList.add('popup__opened');
-  }
-  else {
-    popup.classList.remove('popup__opened');
+    popup.classList.add("popup_opened");
+  } else {
+    popup.classList.remove("popup_opened");
   }
 }
 
-//обработчик отправки "формы"
-form.addEventListener('submit',
-  function (event) {
-      event.preventDefault();
-      nameProfile.textContent = namePopup.value;
-      jobProfile.textContent = jobPopup.value;
-      openPopup();
-  }
-)
+function formSubmitHandler(event) {
+  event.preventDefault();
+  nameProfile.textContent = namePopup.value;
+  jobProfile.textContent = jobPopup.value;
+  togglePopup();
+}
 
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', openPopup);
+//обработчик отправки "формы"
+form.addEventListener("submit", formSubmitHandler);
+
+openPopupButton.addEventListener("click", togglePopup);
+closePopupButton.addEventListener("click", togglePopup);
